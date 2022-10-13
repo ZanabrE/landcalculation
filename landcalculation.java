6a9509da074cc187de876c93310d81a3a52bc19f
G:\Git <ernesto.zanabria1979@gmail.com>
2022-10-12 20:29:35 -0400

    New file landcalcualtion.java

---
+++ landcalculation.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+import java.security.PublicKey;
+
+public class landcalculation{
+    /**
+     * @param args
+     */
+
+     public static void main(String[] args) {
+        
+        
+     }
+}
