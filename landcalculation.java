40c99b1b7e90903636a7b6dca758d4b8a06c7496
G:\Git <ernesto.zanabria1979@gmail.com>
2022-10-12 20:55:24 -0400

    Modified landcalculation.java

--- landcalculation.java
+++ landcalculation.java
@@ -7,7 +7,33 @@ public class landcalculation{
      */
 
      public static void main(String[] args) {
+        //Header.
+        System.out.println("***************************************");
+        System.out.println("*       Land Calculation App          *");
+        System.out.println("***************************************");
         
+        //Variables.
+        double acreLand = 43.560;
+        double tractLand = 389.767;
+        double totalAcreLand;
+        double totalTractLand;
+        double total;
+
+        //Calculation of Acre in Square Feet.
+        totalAcreLand = Math.pow(acreLand, 2.0);
+
+        //Calcualation of Tract in Square Feet.
+        totalTractLand = Math.pow(tractLand, 2.0);
+
+        //Calculations.
+        total = totalTractLand / totalAcreLand;
+
+        //Displaying the total of acre in square feet.
+        System.out.println("The total of an acre of land is " + totalAcreLand + " sq.");
+
+        System.out.println("The total of a tract of land is " + totalTractLand + " sq.");
+
+        System.out.println("The total is " + total + " sq.");
         
-     }
+    }
 }
